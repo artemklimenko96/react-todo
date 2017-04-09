2340c829d62fcd54df4ca9497716becd5e44792e
Artem Klimenko <artemklimenko96@gmail.com>
2017-04-09 19:58:05 +0300

    Added filter functionality

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -47,4 +47,40 @@ describe('TodoAPI', ()=> {
             expect(actualTodos).toEqual(todos);
         });
     });
+    describe('filterTodos', ()=>{
+        var todos = [{
+            id:1,
+            text: 'Smells like teen spirit',
+            completed: 'true'},
+        {   
+            id:2,
+            text: 'something in this way',
+            completed: false
+        }, {
+            id:3,
+            text: 'Heart-shaped box',
+            completed: true
+        }];
+        it('should return all items if  showcompleted is true', ()=>{
+            var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filteredTodos.length).toBe(3);
+        });
+        it('should return all not completed items if showcompleted is false', ()=>{
+            var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+            expect(filteredTodos.length).toBe(1);
+        });
+        it('should sort by completed status', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filteredTodos[0].completed).toBe(false);
+        });
+
+         it('should filter todos by search text', ()=>{
+            var filteredTodos = TodoAPI.filterTodos(todos, true, 'smells');
+            expect(filteredTodos.length).toBe(1);
+        });
+         it('should return all todos if search text is empty', ()=>{
+            var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filteredTodos.length).toBe(3);
+        });
+});
 });
