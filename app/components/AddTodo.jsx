e30c28c056d1fc222e611a1ec6dc1a1bdc250864
Artem Klimenko <artemklimenko96@gmail.com>
2017-04-10 02:03:37 +0300

    Added styling to containers

--- app/components/AddTodo.jsx
+++ app/components/AddTodo.jsx
@@ -13,7 +13,7 @@ var AddTodo = React.createClass({
   },
   render: function (){
     return (
-      <div>
+      <div className = "container__footer">
         <form ref = "form" onSubmit={this.handleSubmit}>
           <input type = "text" ref = "todoText" placeholder = "What o you need to do?"/>
           <button className = "button expanded">Add Todo</button>
