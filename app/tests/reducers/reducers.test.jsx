d5ce69884c5d9bdae86b17148ad51dbef3d9a5c7
Artem Klimenko <artemklimenko96@gmail.com>
2017-04-24 22:12:57 +0300

    Added add todo and toggle todo reducers

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,4 +1,5 @@
 var expect = require('expect');
+var moment = require('moment');
 var reducers = require('reducers');
 var df = require('deep-freeze-strict');
 
@@ -21,5 +22,34 @@ describe('Reducers', () => {
            var res = reducers.showCompletedReducer(df(false), df(action));
            expect(res).toEqual(true); 
         });
+});
+describe('todosReducer', () => {
+    it('should add new todo', () =>{
+        var action = {
+            type:'ADD_TODO',
+            text:'Walk with Aya'
+        };
+        var res = reducers.todoReducer(df([]), df(action));
+        expect(res.length).toEqual(1);
+        expect(res[0].text).toEqual(action.text); 
+    });
+    it('should toggle todos completed state', () => {
+        var todos = [{
+        id: '1212',
+        text: 'Test text',
+        completed: true,
+        createdAt: 123,
+        completedAt:246
+
+    }];
+        var action =  {
+        type: 'TOGGLE_TODO',
+        id:'1212'
+        };
+        var res = reducers.todoReducer(df(todos), df(action));
+        expect(res.length).toEqual(1);
+        expect(res[0].completed).toEqual(false); 
+        expect(res[0].completedAt).toBe(undefined);
+    });
 });
 });
