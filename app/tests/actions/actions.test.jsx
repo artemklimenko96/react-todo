2d37505ac0297dd794160d1c91c1846f1ca0e67f
Artem Klimenko <artemklimenko96@gmail.com>
2017-05-21 17:09:18 +0300

    Added startAddTodos action and test for it, fixed the bug with "nothing todo" message

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -78,43 +78,62 @@ describe('Actions', () =>{
     var res = actions.updateTodo(action.id, action.updates);
     expect(res).toEqual(action);
 });
-describe('tests with firebase todos', () => {
+ describe('Tests with firebase todos', () => {
     var testTodoRef;
+
     beforeEach((done) => {
+      var todosRef = firebaseRef.child('todos');
+
+      todosRef.remove().then(() => {
         testTodoRef = firebaseRef.child('todos').push();
-        
-        testTodoRef.set({
-            text: 'Something todo',
-            completed:false,
-            createdAt: 14785
-        }).then(()=> done());
+
+        return testTodoRef.set({
+          text: 'Something to do',
+          completed: false,
+          createdAt: 23453453
+        })
+      })
+      .then(() => done())
+      .catch(done);
     });
 
-    afterEach((done) =>{
-        testTodoRef.remove().then(()=>done());
+    afterEach((done) => {
+      testTodoRef.remove().then(() => done());
     });
 
-    it('should toggle todo and dispatch UPDATE_TODO action', (done)=>{
-        const store = createMockStore({});
-        const action = actions.startToggleTodo(testTodoRef.key, true);
+    it('should toggle todo and dispatch UPDATE_TODO action', (done) => {
+      const store = createMockStore({});
+      const action = actions.startToggleTodo(testTodoRef.key, true);
 
-        store.dispatch(action).then(()=>{
-            const mockActions = store.getActions();
+      store.dispatch(action).then(() => {
+        const mockActions = store.getActions();
 
-            expect(mockActions[0]).toInclude({
-                type:'UPDATE_TODO',
-                id:testTodoRef.key
-            });
+        expect(mockActions[0]).toInclude({
+          type: 'UPDATE_TODO',
+          id: testTodoRef.key
+        });
+        expect(mockActions[0].updates).toInclude({
+          completed: true
+        });
+        expect(mockActions[0].updates.completedAt).toExist();
 
-            expect(mockActions[0].updates).toInclude({
-                completed:true
-            });
-
-            expect(mockActions[0].updates.completedAt).toExist();
-
-            done();
-        }, ()=> done());
+        done();
+      }, done);
     });
 
-});
+    it('should populate todos and dispatch ADD_TODOS', (done) => {
+      const store = createMockStore({});
+      const action = actions.startAddTodos();
+
+      store.dispatch(action).then(() => {
+        const mockActions = store.getActions();
+
+        expect(mockActions[0].type).toEqual('ADD_TODOS');
+        expect(mockActions[0].todos.length).toEqual(1);
+        expect(mockActions[0].todos[0].text).toEqual('Something to do');
+
+        done();
+      }, done)
+    });
+  });
 });
